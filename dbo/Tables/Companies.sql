20cc755aa0a92bd38e598e8cfb959b2482dd1ed4
WilliamPrakash <willprakash@gmail.com>
2025-08-14 19:02:16 -0400

    Initial commit

---
+++ dbo/Tables/Companies.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Companies] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (50) NULL,
+    [Email] VARCHAR (50)  NULL,
+    [Notes] NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo/Tables/Employees.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [EmployerId] INT            NOT NULL,
+    [FirstName]  VARCHAR (50)   NOT NULL,
+    [LastName]   NVARCHAR (50)  NOT NULL,
+    [Email]      NVARCHAR (50)  NOT NULL,
+    [Occupation] VARCHAR (50)   NOT NULL,
+    [Salary]     DECIMAL (7, 2) NOT NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Employees_Companies] FOREIGN KEY ([EmployerId]) REFERENCES [dbo].[Companies] ([Id])
+);
+
